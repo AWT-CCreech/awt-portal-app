e92966b353250ecbac0e653da01c7a11e18325ee
CCreech <ccreech@airway.com>
2024-07-16 11:10:40 -0400

    Update App.tsx
    Ensure private routes are inaccessible unless user is authenticated

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -5,37 +5,35 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import LoginPage from '../../features/login/LoginPage';
 import Home from '../../features/home/Home';
 import NotFoundPage from '../../features/exception-pages/NotFoundPage';
-import UserInfo from '../../stores/userInfo';
+import UserInfoContext from '../../stores/userInfo';
 import PrivateRoute from '../../sharedComponents/PrivateRoute';
-import TimeTrackerPage from '../../features/time-tracker/TimeTrackerPage';
 import MasterSearch from '../../features/master-search/MasterSearch';
 import DropShip from '../../features/dropship/DropShip';
 import UserListPage from '../../features/user-list/UserListPage';
-import setDocumentTitle from '../../utils/setDocumentTitle'; // Import the utility function
+import setDocumentTitle from '../../utils/setDocumentTitle';
 
 const App: React.FC = () => {
-  const { setUserName, setPassWord } = useContext(UserInfo);
+  const userInfo = useContext(UserInfoContext);
   const location = useLocation();
 
   useEffect(() => {
     setDocumentTitle(location.pathname); // Update the title based on the current pathname
 
     const u = localStorage.getItem('username');
-    if (u !== null) setUserName(u);
+    if (u !== null) userInfo.setUserName(u);
     const p = localStorage.getItem('password');
-    if (p !== null) setPassWord(p);
-  }, [location.pathname, setUserName, setPassWord]); // Add location.pathname as a dependency
+    if (p !== null) userInfo.setPassWord(p);
+  }, [location.pathname, userInfo]);
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPage />} />
       <Route element={<PrivateRoute />}>
-        <Route path="/DropShip" element={<DropShip />} />
-        <Route path="/MassMailer" element={<MassMailer />} />
-        {/* <Route path="/TimeTracker" element={<TimeTrackerPage />} /> */}
-        <Route path="/MasterSearch" element={<MasterSearch />} />
-        <Route path="/UserList" element={<UserListPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/dropship" element={<DropShip />} />
+        <Route path="/massmailer" element={<MassMailer />} />
+        <Route path="/mastersearch" element={<MasterSearch />} />
+        <Route path="/userlist" element={<UserListPage />} />
       </Route>
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
