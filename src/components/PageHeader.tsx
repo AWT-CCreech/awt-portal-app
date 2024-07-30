6be11daa5053ea10f95036b82feafe1354f86723
CCreech <ccreech@airway.com>
2024-07-30 15:44:03 -0400

    Update PageHeader.tsx
    Make component sticky so the PageHeader is always visible

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Grid, Typography, Box } from '@mui/material';
+import { Box, Container, Grid, Typography } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import PortalMenu from './PortalMenu';
 
@@ -12,17 +12,26 @@ const PageHeader: React.FC<IProps> = ({ pageName, pageHref }) => {
   const username = localStorage.getItem('username');
 
   return (
-    <Container maxWidth={false} style={{ padding: '15px 50px', backgroundColor: 'whiteSmoke' }}>
+    <Container
+      maxWidth={false}
+      sx={{
+        padding: '15px 50px',
+        backgroundColor: 'whiteSmoke',
+        position: 'sticky',
+        top: 0,
+        zIndex: 1100, // Ensure it's above other elements
+      }}
+    >
       <Grid container alignItems="center">
         <Grid item xs={6} style={{ display: 'flex', alignItems: 'center' }}>
           <PortalMenu />
-          <Typography variant="h6" component="div" style={{ marginLeft: '15px', color: '#004780' }}>
+          <Typography variant="h6" component="div" style={{ marginLeft: '15px', color: '#384959' }}>
             {pageName}
           </Typography>
         </Grid>
         <Grid item xs={6} style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
           <Box display="flex" alignItems="center">
-            <Typography variant="h6" component="div" style={{ display: 'inline-flex', alignItems: 'center', color: '#004780' }}>
+            <Typography variant="h6" component="div" style={{ display: 'inline-flex', alignItems: 'center', color: '#384959' }}>
               <AccountCircleIcon fontSize="small" style={{ marginRight: '3px' }} />
               {username}
             </Typography>
