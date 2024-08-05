736681681121d56c7295c7ca99744f3cc251b115
CCreech <ccreech@airway.com>
2024-08-05 10:20:47 -0400

    Create SimpleList.ts

---
+++ src/models/SimpleList.ts
@@ -0,0 +1,13 @@
+export interface SimpleList {
+    id: number;
+    ltype?: string;
+    litem?: string;
+    ldesc?: string;
+    ltag?: string;
+    resTech?: number;
+    metalCategory?: string;
+    sort1?: number;
+    sort2?: number;
+}
+  
+  
