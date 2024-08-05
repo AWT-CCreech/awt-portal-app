d2e4e2bf0fc308ee3598007b4de12100188340b4
CCreech <ccreech@airway.com>
2024-08-05 10:21:50 -0400

    Update SearchBox.tsx
    Implement select logic for AccountNumbers and ItemCategories. Implement excel export logic.

--- src/pages/open-so-report/SearchBox.tsx
+++ src/pages/open-so-report/SearchBox.tsx
@@ -1,24 +1,33 @@
 import React, { ChangeEvent, KeyboardEvent, useEffect, useState } from 'react';
-import { Box, Button, TextField, Grid, Checkbox, FormControlLabel, Select, MenuItem, InputLabel, FormControl, SelectChangeEvent } from '@mui/material';
+import {
+  Box, Button, TextField, Grid, Checkbox, FormControlLabel, Select, MenuItem, InputLabel, FormControl, SelectChangeEvent, IconButton, Tooltip
+} from '@mui/material';
+import { GetApp, Search } from '@mui/icons-material';
 import OpenSalesOrderSearchInput from '../../models/OpenSOReport/SearchInput';
+import { AccountNumbers } from '../../models/OpenSOReport/AccountNumbers';
 import { ActiveSalesReps } from '../../models/OpenSOReport/ActiveSalesReps';
 import { ActiveSalesTeams } from '../../models/OpenSOReport/ActiveSalesTeams';
+import { ItemCategories } from '../../models/OpenSOReport/ItemCategories';
 import Modules from '../../app/api/agent';
 
 interface SearchBoxProps {
   searchParams: OpenSalesOrderSearchInput;
   setSearchParams: React.Dispatch<React.SetStateAction<OpenSalesOrderSearchInput>>;
   getResultSets: () => void;
+  handleExport: () => void; 
+  searchResultLength: number;
 }
 
-const SearchBox: React.FC<SearchBoxProps> = ({ searchParams, setSearchParams, getResultSets }) => {
+const SearchBox: React.FC<SearchBoxProps> = ({ searchParams, setSearchParams, getResultSets, handleExport, searchResultLength }) => {
   const [salesReps, setSalesReps] = useState<ActiveSalesReps[]>([]);
   const [salesTeams, setSalesTeams] = useState<ActiveSalesTeams[]>([]);
+  const [itemCategories, setItemCategories] = useState<ItemCategories[]>([]);
+  const [accountNumbers, setAccountNumbers] = useState<AccountNumbers[]>([]);
 
   useEffect(() => {
     const fetchSalesReps = async () => {
       try {
-        const reps = await Modules.SalesReps.fetchActiveSalesReps();
+        const reps = await Modules.OpenSalesOrderReport.fetchActiveSalesReps();
         console.log('Fetched sales reps:', reps); // Log the fetched data
         if (Array.isArray(reps)) {
           setSalesReps(reps);
@@ -32,9 +41,9 @@ const SearchBox: React.FC<SearchBoxProps> = ({ searchParams, setSearchParams, ge
 
     const fetchSalesTeams = async () => {
       try {
-        const teams = await Modules.SalesTeams.fetchActiveSalesTeams();
+        const teams = await Modules.OpenSalesOrderReport.fetchActiveSalesTeams();
         console.log('Fetched sales teams:', teams);
-        if(Array.isArray(teams)) {
+        if (Array.isArray(teams)) {
           setSalesTeams(teams);
         } else {
           console.error('Unexpected data format:', teams);
@@ -44,6 +53,36 @@ const SearchBox: React.FC<SearchBoxProps> = ({ searchParams, setSearchParams, ge
       }
     };
 
+    const fetchAccounts = async () => {
+      try {
+        const accountNumbers = await Modules.OpenSalesOrderReport.fetchAccountNumbers();
+        console.log('Fetched accountNumbers:', accountNumbers);
+        if (Array.isArray(accountNumbers)) {
+          setAccountNumbers(accountNumbers);
+        } else {
+          console.error('Unexpected data format:', accountNumbers);
+        }
+      } catch (error) {
+        console.error('Error fetching account numbers', error);
+      }
+    };
+
+    const fetchCategories = async () => {
+      try {
+        const categories = await Modules.OpenSalesOrderReport.fetchItemCategories();
+        console.log('Fetched categories:', categories);
+        if (Array.isArray(categories)) {
+          setItemCategories(categories);
+        } else {
+          console.error('Unexpected data format:', categories);
+        }
+      } catch (error) {
+        console.error('Error fetching item categories', error);
+      }
+    };
+
+    fetchAccounts();
+    fetchCategories();
     fetchSalesReps();
     fetchSalesTeams();
 
@@ -62,7 +101,7 @@ const SearchBox: React.FC<SearchBoxProps> = ({ searchParams, setSearchParams, ge
     const { name, value } = event.target;
     setSearchParams(prevParams => ({
       ...prevParams,
-      [name]: name === 'date1' || name === 'date2' ? new Date(value) : value,
+      [name]: name === 'date1' || name === 'date2' ? (value ? new Date(value) : null) : value,
     }));
   };
 
@@ -100,8 +139,8 @@ const SearchBox: React.FC<SearchBoxProps> = ({ searchParams, setSearchParams, ge
               value={searchParams.dateFilterType || 'OrderDate'}
               onChange={handleSelectChange}
             >
-              <MenuItem value="OrderDate">Order Date</MenuItem>
-              <MenuItem value="ExpectedDelivery">Expected Delivery</MenuItem>
+              <MenuItem key="OrderDate" value="OrderDate">Sales Order Date</MenuItem>
+              <MenuItem key="ExpectedDelivery" value="ExpectedDelivery">Expected Delivery Date</MenuItem>
             </Select>
           </FormControl>
         </Grid>
@@ -129,7 +168,7 @@ const SearchBox: React.FC<SearchBoxProps> = ({ searchParams, setSearchParams, ge
         </Grid>
 
         <Grid item xs={12} sm={4}>
-        <FormControl fullWidth>
+          <FormControl fullWidth>
             <InputLabel id="salesTeam-label">Sales Team</InputLabel>
             <Select
               labelId="salesTeam-label"
@@ -137,8 +176,9 @@ const SearchBox: React.FC<SearchBoxProps> = ({ searchParams, setSearchParams, ge
               value={searchParams.salesTeam || 'All'}
               onChange={handleSelectChange}
             >
+              <MenuItem key="All" value="All">All</MenuItem>
               {salesTeams.map((team) => (
-                <MenuItem key={team.litem} value={team.litem}>
+                <MenuItem key={team.id} value={team.litem}>
                   {team.litem}
                 </MenuItem>
               ))}
@@ -173,7 +213,7 @@ const SearchBox: React.FC<SearchBoxProps> = ({ searchParams, setSearchParams, ge
               value={searchParams.salesRep || 'All'}
               onChange={handleSelectChange}
             >
-              <MenuItem value="All">All</MenuItem>
+              <MenuItem key="All" value="All">All</MenuItem>
               {salesReps.map((rep) => (
                 <MenuItem key={rep.id} value={rep.uname}>
                   {rep.uname}
@@ -210,11 +250,50 @@ const SearchBox: React.FC<SearchBoxProps> = ({ searchParams, setSearchParams, ge
               value={searchParams.reqDateStatus || 'All'}
               onChange={handleSelectChange}
             >
-              <MenuItem value="All">All</MenuItem>
-              <MenuItem value="Late">Late</MenuItem>
+              <MenuItem key="All" value="All">All</MenuItem>
+              <MenuItem key="Late" value="Late">Late</MenuItem>
             </Select>
           </FormControl>
         </Grid>
+        
+        <Grid item xs={12} sm={4}>
+          <FormControl fullWidth>
+            <InputLabel id="category-label">Category</InputLabel>
+            <Select
+              labelId="category-label"
+              name="category"
+              value={searchParams.category || 'All'}
+              onChange={handleSelectChange}
+            >
+              <MenuItem key="All" value="All">All</MenuItem>
+              {itemCategories.map((category) => (
+                <MenuItem key={category.id} value={category.litem}>
+                  {category.litem}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        
+        <Grid item xs={12} sm={4}>
+          <FormControl fullWidth>
+            <InputLabel id="accountNumber-label">Account Number</InputLabel>
+            <Select
+              labelId="accountNumber-label"
+              name="accountNo"
+              value={searchParams.accountNo || 'All'}
+              onChange={handleSelectChange}
+            >
+              <MenuItem key="All" value="All">All</MenuItem>
+              {accountNumbers.map((accountNumber) => (
+                <MenuItem key={accountNumber.accountNo} value={accountNumber.accountNo}>
+                  {accountNumber.accountNo}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+
         <Grid item xs={12} sm={4}>
           <TextField
             fullWidth
@@ -246,21 +325,34 @@ const SearchBox: React.FC<SearchBoxProps> = ({ searchParams, setSearchParams, ge
             label="All Here"
           />          
           <FormControlLabel
-          control={
-            <Checkbox
-              checked={searchParams.chkGroupBySo || false}
-              onChange={handleCheckboxChange}
-              name="chkGroupBySo"
-            />
-          }
-          label="Group by SO"
-        />
+            control={
+              <Checkbox
+                checked={searchParams.chkGroupBySo || false}
+                onChange={handleCheckboxChange}
+                name="chkGroupBySo"
+              />
+            }
+            label="Group by SO"
+          />
         </Grid>
 
-        <Grid item xs={12}>
-          <Button fullWidth variant="contained" color="primary" onClick={getResultSets}>
+        <Grid item xs={12} display="flex" justifyContent="flex-start" alignItems="center">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={getResultSets}
+            startIcon={<Search />}
+            sx={{ flexGrow: 1 }}
+          >
             Search
           </Button>
+          <Tooltip title="Export to Excel">
+            <span>
+              <IconButton color="success" onClick={handleExport} sx={{ ml: 2 }} disabled={searchResultLength === 0}>
+                <GetApp />
+              </IconButton>
+            </span>
+          </Tooltip>
         </Grid>
       </Grid>
     </Box>
