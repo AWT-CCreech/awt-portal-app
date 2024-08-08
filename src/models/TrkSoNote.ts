655dd63eecdaf5d347bbbc8e66bb61e9510e66e9
CCreech <ccreech@airway.com>
2024-08-08 16:21:46 -0400

    Create TrkSoNote.ts

---
+++ src/models/TrkSoNote.ts
@@ -0,0 +1,12 @@
+export interface TrkSoNote {
+    id: number;
+    orderNo?: string;
+    partNo?: string;
+    enteredBy?: string;
+    entryDate?: Date;
+    modBy?: string;
+    modDate?: Date;
+    noteType?: string;
+    notes?: string;
+    contactId?: number;
+}
