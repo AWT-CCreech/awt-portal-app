b95c483442ab5f1de7c33dc4eeab4fcf29e55012
CCreech <ccreech@airway.com>
2024-09-24 14:45:46 -0400

    Update authentication.ts
    Refactor handleLogOut function to remove redundant parameters, streamline session clearing logic, and ensure proper navigation to the login page. Simplify handleAutoLogout by directly calling onLogout for session management.

--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -1,5 +1,6 @@
 import { NavigateFunction } from 'react-router-dom';
 
+// Check if the user is authenticated
 export const isAuthenticated = () => {
   const storageUser = localStorage.getItem('username');
   const storagePass = localStorage.getItem('password');
@@ -15,18 +16,35 @@ export const isAuthenticated = () => {
   );
 };
 
+// Handle user logout and clear session
 export const handleLogOut = (
   navigate: NavigateFunction,
-  setUserName: (u: string) => void,
-  setPassWord: (p: string) => void
-) => {
+  setUserName: Function,
+  setPassWord: Function
+): void => {
+  console.log('Logging out');
   localStorage.removeItem('token');
-  localStorage.removeItem('expiresAt');
   localStorage.removeItem('username');
   localStorage.removeItem('password');
   localStorage.removeItem('userid');
-
+  localStorage.removeItem('expiresAt');
+  localStorage.removeItem('lastRefresh');
   setUserName('');
   setPassWord('');
+
+  // Set document title to Login when logging out
+  document.title = 'AWT Portal | Login';
+
   navigate('/login');
+};
+
+// Auto logout due to inactivity or token expiration
+export const handleAutoLogout = (
+  navigate: NavigateFunction,
+  onLogout: Function,
+  setPassWord: Function
+) => {
+  console.log('Auto logout triggered due to inactivity');
+  onLogout(); // Clear session and perform any additional logout logic
+  navigate('/login'); // Redirect to the login page
 };
