3e1b5c9d1b51cb2b68880715f313ae9c43d4d0a3
CCreech <ccreech@airway.com>
2024-10-11 13:27:09 -0400

    Update SearchInput.ts
    - Set `SearchInput` interface as the default export for streamlined imports
    - Removed duplicate `YearRange` field in `SearchInput` interface

--- src/models/PODeliveryLog/SearchInput.ts
+++ src/models/PODeliveryLog/SearchInput.ts
@@ -1,13 +1,13 @@
-export interface SearchInput {
+export default interface SearchInput {
   PONum: string;
   Vendor: string;
   PartNum: string;
   IssuedBy: string;
   SONum: string;
   xSalesRep: string;
-  HasNotes: string; 
+  HasNotes: string;
   POStatus: string;
   EquipType: string;
   CompanyID: string;
-  YearRange: number; 
+  YearRange: number;
 }
