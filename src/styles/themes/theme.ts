26d7a064ee6fcd5e5b634a2431fc9f17517247e4
CCreech <ccreech@airway.com>
2024-10-11 13:27:19 -0400

    Create theme.ts

---
+++ src/styles/themes/theme.ts
@@ -0,0 +1,6 @@
+import { createTheme } from '@mui/material/styles';
+import { themeOptions } from './themeOptions'; // adjust the path as necessary
+
+const theme = createTheme(themeOptions);
+
+export default theme;
