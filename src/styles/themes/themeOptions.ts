f0f680212f3ad3733f9e0ce581c038f4d7ae6ac0
CCreech <ccreech@airway.com>
2024-10-11 13:27:22 -0400

    Create themeOptions.ts

---
+++ src/styles/themes/themeOptions.ts
@@ -0,0 +1,63 @@
+// themeOptions.ts
+import { ThemeOptions } from '@mui/material/styles';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light', // 'mode' is used instead of 'type' in MUI v5
+    primary: {
+      main: '#06477d',
+      light: '#376b97',
+      dark: '#043157',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#78909c',
+      light: '#93a6af',
+      dark: '#54646d',
+      contrastText: '#ffffff',
+    },
+    info: {
+      main: '#90caf9',
+      light: '#a6d4fa',
+      dark: '#648dae',
+      contrastText: 'rgba(0,0,0,0.87)',
+    },
+    warning: {
+      main: '#ff9800',
+      dark: '#f57c00',
+      light: '#ffac33',
+      contrastText: 'rgba(0,0,0,0.87)',
+    },
+    text: {
+      primary: 'rgba(0,0,0,0.87)',
+      secondary: 'rgba(0,0,0,0.54)',
+      disabled: 'rgba(0,0,0,0.38)',
+    },
+    error: {
+      main: '#f44336',
+      light: '#f6685e',
+      dark: '#aa2e25',
+      contrastText: '#ffffff',
+    },
+    success: {
+      main: '#4caf50',
+      light: '#6fbf73',
+      dark: '#357a38',
+      contrastText: 'rgba(0,0,0,0.87)',
+    },
+    divider: 'rgba(0,0,0,0.12)',
+  },
+  typography: {
+    fontSize: 14,
+  },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        colorInherit: {
+          backgroundColor: '#689f38',
+          color: '#fff',
+        },
+      },
+    },
+  },
+};
