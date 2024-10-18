010311b2e1a5d0599580a937c461ea9843f0cae6
CCreech <ccreech@airway.com>
2024-10-18 10:27:44 -0400

    Update setDocumentTitle.ts
    - Replaced switch-case logic with `DOCUMENT_TITLES` mapping for setting document titles based on the route path
    - Simplified `setDocumentTitle` function by directly using the `DOCUMENT_TITLES` object for route-to-title mapping

--- src/utils/setDocumentTitle.ts
+++ src/utils/setDocumentTitle.ts
@@ -1,36 +1,22 @@
+import { ROUTE_PATHS } from '../routes'; // Adjust the import path if necessary
+
+// Define a mapping from path to document title
+const DOCUMENT_TITLES: { [key: string]: string } = {
+  [ROUTE_PATHS.HOME]: 'AWT Portal | Home',
+  [ROUTE_PATHS.LOGIN]: 'AWT Portal | Login',
+  [ROUTE_PATHS.PURCHASING.MASS_MAILER]: 'AWT Portal | Mass Mailer',
+  [ROUTE_PATHS.TIME_TRACKER]: 'AWT Portal | Time Tracker',
+  [ROUTE_PATHS.MASTER_SEARCH]: 'AWT Portal | Master Search',
+  [ROUTE_PATHS.SALES.OPEN_SO_REPORT]: 'AWT Portal | Open SO Report',
+  [ROUTE_PATHS.PURCHASING.PO_DELIVERY_LOG]: 'AWT Portal | PO Delivery Log',
+  [ROUTE_PATHS.PURCHASING.DROPSHIP]: 'AWT Portal | Drop Ship',
+  [ROUTE_PATHS.USER_LIST]: 'AWT Portal | User List',
+  // Add other paths and their titles as needed
+};
+
 const setDocumentTitle = (path: string): void => {
-    switch (path) {
-      case '/':
-        document.title = 'AWT Portal | Home';
-        break;
-      case '/login':
-        document.title = 'AWT Portal | Login';
-        break;
-      case '/massmailer':
-        document.title = 'AWT Portal | Mass Mailer';
-        break;
-      case '/timetracker':
-        document.title = 'AWT Portal | Time Tracker';
-        break;
-      case '/mastersearch':
-        document.title = 'AWT Portal | Master Search';
-        break;
-      case '/opensalesorderreport':
-          document.title = 'AWT Portal | Open SO Report';
-          break;
-      case '/podeliverylog':
-          document.title = 'AWT Portal | PO Delivery Log';
-          break;
-      case '/dropship':
-        document.title = 'AWT Portal | Drop Ship';
-        break;
-      case '/userlist':
-        document.title = 'AWT Portal | User List';
-        break;
-      default:
-        document.title = 'AWT Portal | Menu';
-    }
-  };
-  
+  const title = DOCUMENT_TITLES[path] || 'AWT Portal | Menu';
+  document.title = title;
+};
+
 export default setDocumentTitle;
-  
