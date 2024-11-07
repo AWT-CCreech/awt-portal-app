f3163cad51ca973501bd9e858d06426325f29fb2
CCreech <ccreech@airway.com>
2024-11-07 10:56:22 -0500

    Create EquipmentRequestUpdateDto.ts

---
+++ src/models/SOWorkbench/EquipmentRequestUpdateDto.ts
@@ -0,0 +1,8 @@
+export interface EquipmentRequestUpdateDto {
+    id: number;
+    rwSalesOrderNum: string;
+    username: string;
+    password: string;
+    subject: string;
+    htmlBody: string;
+}
