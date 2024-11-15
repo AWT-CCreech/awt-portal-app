a770f112616bd372c26045772dbfc10e4ea52f96
CCreech <ccreech@airway.com>
2024-11-15 16:17:21 -0500

    Create DetailLevelRowData.ts

---
+++ src/models/SOWorkbench/DetailLevelRowData.ts
@@ -0,0 +1,13 @@
+export interface DetailLevelRowData {
+    id: number; // EquipmentRequest ID
+    saleId: number;
+    requestId: number;
+    rwsalesOrderNum: string;
+    qtySold: number;
+    unitMeasure: string;
+    partNum: string;
+    partDesc: string;
+    unitPrice: number;
+    salesRep: string;
+    dropShipment: boolean;
+}
