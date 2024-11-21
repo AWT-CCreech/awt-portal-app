f2efd5667410cfcd78c5b76ce3447b43e5e97e4d
CCreech <ccreech@airway.com>
2024-11-21 16:01:55 -0500

    Delete InventoryRulesRequestDto.ts

--- src/models/Utility/InventoryRulesRequestDto.ts
+++
@@ -1,9 +0,0 @@
-export interface InventoryRulesRequestDto {
-    RequestID: number;
-    PartNum: string;
-    AltPartNum?: string;
-    Quantity: number;
-    QtySold: number;
-    CallDateRange: number;
-    RequestStatus: string;
-}
