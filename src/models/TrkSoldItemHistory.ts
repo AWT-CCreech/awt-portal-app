7e44a2c92e492b716a120ed3449f0f0e077321a5
CCreech <ccreech@airway.com>
2024-11-21 11:31:50 -0500

    Create TrkSoldItemHistory.ts

---
+++ src/models/TrkSoldItemHistory.ts
@@ -0,0 +1,7 @@
+export interface TrkSoldItemHistory {
+    RowId: number;
+    RequestId?: number;
+    DateMarkedBought?: string; // ISO date string
+    Username?: string;
+    PageName?: string;
+}
