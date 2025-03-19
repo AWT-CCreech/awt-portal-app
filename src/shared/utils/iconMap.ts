49230e2c76911e16bd56dec0e2e52e7bfe13898e
CCreech <ccreech@airway.com>
2025-03-19 15:15:22 -0400

    Create iconMap.ts

---
+++ src/shared/utils/iconMap.ts
@@ -0,0 +1,31 @@
+import TravelExploreOutlinedIcon from '@mui/icons-material/TravelExploreOutlined';
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import PinDropOutlinedIcon from '@mui/icons-material/PinDropOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
+import SellOutlinedIcon from '@mui/icons-material/SellOutlined';
+import MarkunreadMailboxOutlinedIcon from '@mui/icons-material/MarkunreadMailboxOutlined';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import HandymanOutlinedIcon from '@mui/icons-material/HandymanOutlined';
+import TodayOutlinedIcon from '@mui/icons-material/TodayOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import FolderIcon from '@mui/icons-material/Folder';
+
+const iconMap: { [key: string]: React.ElementType } = {
+    TravelExploreOutlinedIcon,
+    PeopleOutlinedIcon,
+    PinDropOutlinedIcon,
+    EmailOutlinedIcon,
+    ContactsOutlinedIcon,
+    SellOutlinedIcon,
+    MarkunreadMailboxOutlinedIcon,
+    ManageSearchIcon,
+    HandymanOutlinedIcon,
+    TodayOutlinedIcon,
+    ShoppingCartOutlinedIcon,
+    FolderIcon,
+};
+
+export const getIconComponent = (iconName?: string): React.ElementType => {
+    return iconMap[iconName || 'FolderIcon'] || FolderIcon;
+};
