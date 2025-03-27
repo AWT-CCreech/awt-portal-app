9fa4c0ac547ab88b1eae39d467362a3c7b42235e
CCreech <ccreech@airway.com>
2025-03-27 09:14:18 -0400

    Update PrivateRoute.tsx

--- src/shared/components/PrivateRoute.tsx
+++ src/shared/components/PrivateRoute.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Navigate, useLocation, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { isAuthenticated } from '../utils/authentication';
 
 const PrivateRoute: React.FC = () => {
   const location = useLocation();
-
-  return isAuthenticated() ? (
-    <Outlet />
-  ) : (
-    <Navigate to="/login" state={{ from: location }} replace />
-  );
+  return isAuthenticated()
+    ? <Outlet />
+    : <Navigate to="/login" state={{ from: location }} replace />;
 };
 
 export default PrivateRoute;
