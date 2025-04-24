673166d1ad4b6f4aa8650c0e04ea9fe505965a85
CCreech <ccreech@airway.com>
2025-04-24 15:00:52 -0400

    Update SearchBox.tsx

--- src/pages/scan-history/SearchBox.tsx
+++ src/pages/scan-history/SearchBox.tsx
@@ -1,14 +1,16 @@
-// src/components/ScanHistory/SearchBox.tsx
-import React, { ChangeEvent, useEffect, useCallback } from 'react';
+import React, { ChangeEvent, useEffect, useCallback, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import Box from '@mui/material/Box';
 import Grid2 from '@mui/material/Grid2';
 import SearchIcon from '@mui/icons-material/Search';
 import LoadingIconButton from '../../shared/components/LoadingIconButton';
+import SharedDateRangePicker from '../../shared/components/DateRangePicker';
 
 import { SearchScansDto, createDefaultSearchScansDto } from '../../models/ScanHistoryModels/SearchScansDto';
 import { User } from '../../models/User';
@@ -28,6 +30,20 @@ const SearchBox: React.FC<SearchBoxProps> = ({
     onSearch,
     loading
 }) => {
+    const [dateRange, setDateRange] = useState({
+        startDate: searchParams.scanDateRangeStart,
+        endDate: searchParams.scanDateRangeEnd,
+    });
+
+    const handleDateRangeChange = (newRange: { startDate: Date; endDate: Date }) => {
+        setDateRange(newRange);
+        setSearchParams(prev => ({
+            ...prev,
+            scanDateRangeStart: newRange.startDate,
+            scanDateRangeEnd: newRange.endDate,
+        }));
+    };
+
     useEffect(() => {
         setSearchParams(createDefaultSearchScansDto());
     }, [setSearchParams]);
@@ -36,11 +52,7 @@ const SearchBox: React.FC<SearchBoxProps> = ({
         const { name, value } = e.target;
         setSearchParams(prev => ({
             ...prev,
-            [name]: name === 'limit'
-                ? parseInt(value, 10)
-                : (name === 'scanDateRangeStart' || name === 'scanDateRangeEnd')
-                    ? new Date(value)
-                    : value
+            [name]: name === 'limit' ? parseInt(value, 10) : value
         }));
     };
 
@@ -64,31 +76,19 @@ const SearchBox: React.FC<SearchBoxProps> = ({
     return (
         <form onSubmit={handleFormSubmit}>
             <Box sx={{ mb: 3, p: 2, border: '1px solid #ddd', borderRadius: 2 }}>
-                <Grid2 container spacing={2} columns={{ xs: 12, sm: 5, md: 5 }}>
+                <Grid2 container spacing={2} columns={{ xs: 12, sm: 4, md: 4 }}>
 
-                    {/* Row 1: Scan Start, Scan End, Scan User, Order Type, Order No */}
+                    {/* 1) Date Range Picker */}
                     <Grid2 size={{ xs: 12, sm: 1, md: 1 }}>
-                        <TextField
-                            fullWidth
-                            label="Scan Start Date"
-                            name="scanDateRangeStart"
-                            type="date"
-                            value={searchParams.scanDateRangeStart.toISOString().split('T')[0]}
-                            onChange={handleInputChange}
-                            slotProps={{ inputLabel: { shrink: true } }}
+                        <SharedDateRangePicker
+                            value={dateRange}
+                            onChange={handleDateRangeChange}
+                            startLabel="Scan Start"
+                            endLabel="Scan End"
                         />
                     </Grid2>
-                    <Grid2 size={{ xs: 12, sm: 1, md: 1 }}>
-                        <TextField
-                            fullWidth
-                            label="Scan End Date"
-                            name="scanDateRangeEnd"
-                            type="date"
-                            value={searchParams.scanDateRangeEnd.toISOString().split('T')[0]}
-                            onChange={handleInputChange}
-                            slotProps={{ inputLabel: { shrink: true } }}
-                        />
-                    </Grid2>
+
+                    {/* 2) Scan User */}
                     <Grid2 size={{ xs: 12, sm: 1, md: 1 }}>
                         <FormControl fullWidth variant="outlined">
                             <InputLabel id="scanUser-label">Scan User</InputLabel>
@@ -108,72 +108,83 @@ const SearchBox: React.FC<SearchBoxProps> = ({
                             </Select>
                         </FormControl>
                     </Grid2>
+
+                    {/* 3) Order Type + No */}
                     <Grid2 size={{ xs: 12, sm: 1, md: 1 }}>
                         <FormControl fullWidth variant="outlined">
-                            <InputLabel id="orderType-label">Order Type</InputLabel>
-                            <Select
-                                labelId="orderType-label"
-                                name="orderType"
-                                value={searchParams.orderType}
-                                onChange={handleSelectChange}
-                                label="Order Type"
-                            >
-                                <MenuItem value=""><em>All</em></MenuItem>
-                                {['SO', 'PO', 'RMA', 'RTV/C'].map(opt => (
-                                    <MenuItem key={opt} value={opt}>{opt}</MenuItem>
-                                ))}
-                            </Select>
+                            <InputLabel htmlFor="order-no-input">Order</InputLabel>
+                            <OutlinedInput
+                                id="order-no-input"
+                                name="orderNum"
+                                value={searchParams.orderNum}
+                                onChange={handleInputChange}
+                                label="Order"
+                                startAdornment={
+                                    <InputAdornment position="start" sx={{ mr: 1 }}>
+                                        <Select
+                                            name="orderType"
+                                            value={searchParams.orderType}
+                                            onChange={handleSelectChange}
+                                            displayEmpty
+                                            size="small"
+                                            sx={{ '& .MuiSelect-select': { py: 1 } }}
+                                            renderValue={(v) => v || 'All'}
+                                        >
+                                            <MenuItem value=""><em>All</em></MenuItem>
+                                            {['SO', 'PO', 'RMA', 'RTV/C'].map(opt => (
+                                                <MenuItem key={opt} value={opt}>{opt}</MenuItem>
+                                            ))}
+                                        </Select>
+                                    </InputAdornment>
+                                }
+                            />
                         </FormControl>
                     </Grid2>
-                    <Grid2 size={{ xs: 12, sm: 1, md: 1 }}>
-                        <TextField
-                            fullWidth
-                            label="Order No"
-                            name="orderNum"
-                            type="text"
-                            value={searchParams.orderNum}
-                            onChange={handleInputChange}
-                        />
-                    </Grid2>
 
-                    {/* Row 2: Part No, Serial Number Field, Serial No, MNS Company, Result Limit */}
+                    {/* 4) Part No */}
                     <Grid2 size={{ xs: 12, sm: 1, md: 1 }}>
                         <TextField
                             fullWidth
                             label="Part No"
                             name="partNo"
-                            type="text"
                             value={searchParams.partNo}
                             onChange={handleInputChange}
                         />
                     </Grid2>
+
+                    {/* 5) Serial Identifier */}
                     <Grid2 size={{ xs: 12, sm: 1, md: 1 }}>
                         <FormControl fullWidth variant="outlined">
-                            <InputLabel id="snField-label">Serial Number Field</InputLabel>
-                            <Select
-                                labelId="snField-label"
-                                name="snField"
-                                value={searchParams.snField}
-                                onChange={handleSelectChange}
-                                label="Serial Number Field"
-                            >
-                                <MenuItem value=""><em>All</em></MenuItem>
-                                {['HeciCode', 'SerialNo', 'SerialNoB'].map(opt => (
-                                    <MenuItem key={opt} value={opt}>{opt}</MenuItem>
-                                ))}
-                            </Select>
+                            <InputLabel htmlFor="serial-no-input">Serial Identifier</InputLabel>
+                            <OutlinedInput
+                                id="serial-no-input"
+                                name="serialNo"
+                                value={searchParams.serialNo}
+                                onChange={handleInputChange}
+                                label="Serial Identifier"
+                                startAdornment={
+                                    <InputAdornment position="start" sx={{ mr: 1 }}>
+                                        <Select
+                                            name="snField"
+                                            value={searchParams.snField}
+                                            onChange={handleSelectChange}
+                                            displayEmpty
+                                            size="small"
+                                            sx={{ '& .MuiSelect-select': { py: 1 } }}
+                                            renderValue={(v) => v || 'All'}
+                                        >
+                                            <MenuItem value=""><em>All</em></MenuItem>
+                                            {['HeciCode', 'SerialNo', 'SerialNoB'].map(opt => (
+                                                <MenuItem key={opt} value={opt}>{opt}</MenuItem>
+                                            ))}
+                                        </Select>
+                                    </InputAdornment>
+                                }
+                            />
                         </FormControl>
                     </Grid2>
-                    <Grid2 size={{ xs: 12, sm: 1, md: 1 }}>
-                        <TextField
-                            fullWidth
-                            label="Serial No"
-                            name="serialNo"
-                            type="text"
-                            value={searchParams.serialNo}
-                            onChange={handleInputChange}
-                        />
-                    </Grid2>
+
+                    {/* 6) MNS Company */}
                     <Grid2 size={{ xs: 12, sm: 1, md: 1 }}>
                         <FormControl fullWidth variant="outlined">
                             <InputLabel id="mnsCo-label">MNS Company</InputLabel>
@@ -191,6 +202,8 @@ const SearchBox: React.FC<SearchBoxProps> = ({
                             </Select>
                         </FormControl>
                     </Grid2>
+
+                    {/* 7) Result Limit */}
                     <Grid2 size={{ xs: 12, sm: 1, md: 1 }}>
                         <TextField
                             fullWidth
@@ -202,8 +215,11 @@ const SearchBox: React.FC<SearchBoxProps> = ({
                         />
                     </Grid2>
 
-                    {/* Search button (unchanged) */}
-                    <Grid2 size={{ xs: 12 }} sx={{ display: 'flex', alignItems: 'center' }}>
+                    {/* 8) Search Button (wraps to its own row) */}
+                    <Grid2
+                        size={{ xs: 12, sm: 4, md: 4 }}
+                        sx={{ display: 'flex', justifyContent: 'flex-start', mt: 1 }}
+                    >
                         <LoadingIconButton
                             type="submit"
                             text="Search"
@@ -213,7 +229,6 @@ const SearchBox: React.FC<SearchBoxProps> = ({
                             sx={{ minWidth: 150, height: 40 }}
                         />
                     </Grid2>
-
                 </Grid2>
             </Box>
         </form>
